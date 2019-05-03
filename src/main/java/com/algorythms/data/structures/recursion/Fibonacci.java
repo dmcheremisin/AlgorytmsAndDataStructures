2fbf08b8e01dd6ef5020b8b48d32b1ac3de169d1
dmitrii89 <dmitry_cher@mail.ru>
2019-05-03 13:09:42 +0300

    Fibonacci recursion

---
+++ src/main/java/com/algorythms/data/structures/recursion/Fibonacci.java
@@ -0,0 +1,40 @@
+package com.algorythms.data.structures.recursion;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        System.out.println(fiboIterative(7)); // 13
+        System.out.println(fiboRecursive(7)); // 13
+    }
+
+    // f0 = 0
+    // f1 = 1
+    // f2 = 1 = 1 + 0
+    // f3 = 2 = 1 + 1 = 1 + 1 + 0
+    // f4 = 3 = 2 + 1 = 1 + 1 + 0 + 1 + 0
+    // f5 = 5 = 3 + 2 = 1 + 1 + 0 + 1 + 0 + 1 + 1 + 0
+    // f(n) = f(n - 1) + f(n - 2)
+
+    public static int fiboRecursive(int num) {
+        if(num == 0) return 0;
+        if(num == 1) return 1;
+        return fiboRecursive(num - 1) + fiboRecursive(num - 2);
+    }
+
+    public static int fiboIterative(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        int counter = 1;
+        int n1 = 0;
+        int n2 = 1;
+        int result = 1;
+        while (counter < num) {
+            result = n1 + n2;
+            n1 = n2;
+            n2 = result;
+            counter++;
+        }
+        return result;
+    }
+}
