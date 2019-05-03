fef61944b054c467d27b197393388572cf9c9832
dmitrii89 <dmitry_cher@mail.ru>
2019-05-03 12:22:31 +0300

    Shell sort implementation

---
+++ src/main/java/com/algorythms/data/structures/sortAlgorithms/ShellSort.java
@@ -0,0 +1,27 @@
+package com.algorythms.data.structures.sortAlgorithms;
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] arr = {45, 35, -15, 7, 55, 1, -22};
+
+        for(int gap = arr.length / 2; gap > 0; gap /= 2) {
+
+            for(int i = gap; i < arr.length; i++) {
+                int newElement = arr[i];
+                int j = i;
+
+                while(j >= gap && arr[j - gap] > newElement) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                arr[j] = newElement;
+            }
+        }
+
+        System.out.println(">>>>>> Result: ");
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
