91bc81510a67b986c73df1da6911389096a73fd7
dmitrii89 <dmitry_cher@mail.ru>
2019-05-20 14:10:39 +0300

    Doubly Linked list addBefore challenge

--- src/main/java/com/algorithms/data/structures/lists/Employee.java
+++ src/main/java/com/algorithms/data/structures/lists/Employee.java
@@ -44,4 +44,24 @@ public class Employee {
                 ", lastName='" + lastName + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null) return false;
+        return lastName != null ? lastName.equals(employee.lastName) : employee.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
 }
---
+++ src/main/java/com/algorithms/data/structures/lists/challenges/addBeforeExisting/DoublyLinkedListTest.java
@@ -0,0 +1,42 @@
+package com.algorithms.data.structures.lists.challenges.addBeforeExisting;
+
+import com.algorithms.data.structures.lists.Employee;
+
+public class DoublyLinkedListTest {
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        System.out.println(list.isEmpty()); // true
+
+        Employee johnSnow = new Employee(25, "John", "Snow");
+        Employee dainerisTargarien = new Employee(38, "Daineris", "Targarien");
+        Employee tyronLannister = new Employee(41, "Tyron", "Lannister");
+
+        list.add(johnSnow);
+        list.add(dainerisTargarien);
+        list.add(tyronLannister);
+
+        list.printList();
+//        HEAD -> Employee{id=25, firstName='John', lastName='Snow'}
+//        NEXT <=> Employee{id=38, firstName='Daineris', lastName='Targarien'}
+//        NEXT <=> Employee{id=41, firstName='Tyron', lastName='Lannister'}
+//        NEXT <=> null
+
+        Employee cerseiLannister = new Employee(43, "Cersei", "Lannister");
+        Employee sansaStark = new Employee(43, "Sansa", "Stark");
+
+        list.addBefore(cerseiLannister, johnSnow);
+        list.addBefore(sansaStark, tyronLannister);
+
+        list.printList();
+//        HEAD -> Employee{id=43, firstName='Cersei', lastName='Lannister'}
+//        NEXT <=> Employee{id=25, firstName='John', lastName='Snow'}
+//        NEXT <=> Employee{id=38, firstName='Daineris', lastName='Targarien'}
+//        NEXT <=> Employee{id=43, firstName='Sansa', lastName='Stark'}
+//        NEXT <=> Employee{id=41, firstName='Tyron', lastName='Lannister'}
+//        NEXT <=> null
+
+        boolean result = list.addBefore(sansaStark, new Employee(23, "Miss", "Andeya"));
+        System.out.println(result); // false
+    }
+}
