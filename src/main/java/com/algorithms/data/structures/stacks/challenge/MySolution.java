d66d8da41465b989dbf71e50da5d4c0aaecdfcba
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-06-02 15:25:16 +0300

    Check palindrome using Stack

---
+++ src/main/java/com/algorithms/data/structures/stacks/challenge/MySolution.java
@@ -0,0 +1,36 @@
+package com.algorithms.data.structures.stacks.challenge;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+// check palindrome using stack or FIFO principle
+
+public class MySolution {
+
+    public static void main(String[] args) {
+        // should return true
+        System.out.println(checkForPalindrome("abccba"));
+        // should return true
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw?"));
+        // should return true
+        System.out.println(checkForPalindrome("I did, did I?"));
+        // should return false
+        System.out.println(checkForPalindrome("hello"));
+        // should return true
+        System.out.println(checkForPalindrome("Don't nod"));
+    }
+
+    private static boolean checkForPalindrome(String input) {
+        input = input.replaceAll("\\W", "");
+        Deque<Character> list = new LinkedList<>();
+        for (char aChar : input.toCharArray()) {
+            list.push(aChar);
+        }
+        StringBuilder reverseInput = new StringBuilder();
+        while (!list.isEmpty()) {
+            reverseInput.append(list.pop());
+        }
+        return reverseInput.toString().equalsIgnoreCase(input);
+    }
+
+}
