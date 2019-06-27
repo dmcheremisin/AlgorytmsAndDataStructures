ef4843fa911e26bddf60f1dec97a3e4ca877b975
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-06-27 15:21:45 +0300

    Heap delete method

--- src/main/java/com/algorithms/data/structures/heaps/Heap.java
+++ src/main/java/com/algorithms/data/structures/heaps/Heap.java
@@ -10,19 +10,76 @@ public class Heap {
 
     private void fixHeapAbove(int index) {
         Integer value = heap[index];
-        while (index > 0 && value > heap[getParentIndex(index)]) {
-            heap[index] = heap[getParentIndex(index)];
-            index = getParentIndex(index);
+        while (index > 0 && value > heap[getParent(index)]) {
+            heap[index] = heap[getParent(index)];
+            index = getParent(index);
         }
         heap[index] = value;
     }
 
-    private int getParentIndex(int index) {
+    private int getParent(int index) {
         return (index - 1) / 2;
+    }
+
+    private int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
     }
 
     public boolean isFull() {
         return size == heap.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int delete(int index) {
+        if(isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+        if(index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        } else {
+            fixHeapAbove(index);
+        }
+
+        size--;
+        return deletedValue;
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+
+        while(index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+            if(leftChild <= lastHeapIndex) {
+                if(rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                }
+                else {
+                    childToSwap = heap[leftChild] > heap[rightChild] ? leftChild : rightChild;
+                }
+
+                if(heap[index] < heap[childToSwap]) {
+                    int tmp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = tmp;
+                } else {
+                    break;
+                }
+                index = childToSwap;
+            }
+            else {
+                break;
+            }
+        }
     }
 
     public void insert(int value) {
@@ -36,8 +93,8 @@ public class Heap {
     }
 
     public void printHeap() {
-        for (Integer integer : heap) {
-            System.out.print(integer + ", ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i] + ", ");
         }
         System.out.println();
 
--- src/main/java/com/algorithms/data/structures/heaps/HeapTest.java
+++ src/main/java/com/algorithms/data/structures/heaps/HeapTest.java
@@ -4,13 +4,23 @@ public class HeapTest {
 
     public static void main(String[] args) {
 
-        Heap heap = new Heap(10);
-        heap.insert(20);
-        heap.insert(12);
-        heap.insert(34);
-        heap.insert(45);
-        heap.insert(10);
+        Heap heap = new Heap(8);
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
 
-        heap.printHeap(); // 45, 34, 20, 12, 10, null, null, null, null, null,
+        heap.printHeap(); // 80, 75, 60, 68, 55, 40, 52, 67,
+
+//        heap.delete(1);
+//        heap.printHeap(); // 80, 68, 60, 67, 55, 40, 52,
+
+        heap.delete(0);
+        heap.printHeap(); // 75, 68, 60, 67, 55, 40, 52,
     }
+
 }
