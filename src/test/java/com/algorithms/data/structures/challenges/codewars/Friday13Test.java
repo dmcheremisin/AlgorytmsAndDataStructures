2420955538b454e69499be446800239bc13d92ad
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-08-12 23:25:41 +0300

    Friday 13

---
+++ src/test/java/com/algorithms/data/structures/challenges/codewars/Friday13Test.java
@@ -0,0 +1,14 @@
+package com.algorithms.data.structures.challenges.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Friday13Test {
+
+    @Test
+    public void unluckyDays() {
+        assertEquals(3, Friday13.unluckyDays(2015));
+        assertEquals(1, Friday13.unluckyDays(1986));
+    }
+}
