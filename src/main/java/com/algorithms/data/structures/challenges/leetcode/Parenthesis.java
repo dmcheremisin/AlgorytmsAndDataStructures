ab8e9f62a13914743131bff21ac2ad2562bb006c
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-09-02 00:23:01 +0300

    Parenthesis

--- src/main/java/com/algorithms/data/structures/challenges/leetcode/Parenthesis.java
+++ src/main/java/com/algorithms/data/structures/challenges/leetcode/Parenthesis.java
@@ -41,48 +41,28 @@ package com.algorithms.data.structures.challenges.leetcode;
 // 2(border index, no more combinations on it) =
 //      2n - 2(last index) - (n - 1)(elements to store in string, first always 0) = n - 1
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Parenthesis {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 2; i++)
-        recursiveGetNums(1, 1, 3);
+        new Parenthesis().generateParenthesis(4);
     }
 
-    private static void recursiveGetNums(int idx, int counter, int nums) {
-        System.out.println("Index is " + idx + " counter " + counter);
-        if (counter++ < nums - 1)
-            recursiveGetNums(idx + 1, nums, counter);
-
-        for (int i = idx + 1; i < nums; i++)
-            System.out.println("Parent index: " + idx + " ; current index: " + i);
-    }
-
-    public static List<String> generateParenthesis(int n) {
-        int length = 2 * n;
-        Set<List<Integer>> sets = new HashSet<>();
-
-        int i = 1;
-        while (i < n + 1) {
-            System.out.println(i);
-            i++;
-        }
+    public List<String> generateParenthesis(int n) {
+        int lastIndex = 2 * n - 1;
+        getIntegers(0, 0, n, lastIndex);
         return null;
     }
 
-    public List<Integer> getIndexes(int initialIndex, int n) {
-        List<Integer> nums = new ArrayList<>();
-        int i = initialIndex;
-        while (i < 2 * n - 2) {
-            nums.add(i);
-            i++;
+    private void getIntegers(int itemNumber, int position, int n, int lastIndex) {
+        if (itemNumber >= n || position >= (lastIndex - 1))
+            return;
+        System.out.println("========> Item number: " + itemNumber);
+        int borderIndex = lastIndex - 2 * (n - itemNumber) + 1;
+        for (int i = position; i <= borderIndex; i++) {
+            System.out.println("Item number: " + itemNumber + " position: " + i);
+            getIntegers(itemNumber + 1, position + 1, n, lastIndex);
         }
-
-        return nums;
     }
-
 }
