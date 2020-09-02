61fef19a0aeb9ae4c08f522a31adbca52edef04b
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-09-03 00:39:44 +0300

    Parenthesis

--- src/main/java/com/algorithms/data/structures/challenges/leetcode/Parenthesis.java
+++ src/main/java/com/algorithms/data/structures/challenges/leetcode/Parenthesis.java
@@ -41,28 +41,51 @@ package com.algorithms.data.structures.challenges.leetcode;
 // 2(border index, no more combinations on it) =
 //      2n - 2(last index) - (n - 1)(elements to store in string, first always 0) = n - 1
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Parenthesis {
 
     public static void main(String[] args) {
-        new Parenthesis().generateParenthesis(4);
+        List<String> strings = new Parenthesis().generateParenthesis(4);
+        strings.forEach(System.out::println);
     }
 
     public List<String> generateParenthesis(int n) {
         int lastIndex = 2 * n - 1;
-        getIntegers(0, 0, n, lastIndex);
-        return null;
+
+        Set<Set<Integer>> setOfSets = new HashSet<>();
+        getIntegers(0, 0, n, lastIndex, new HashSet<>(), setOfSets);
+
+        return convertSetsOfIntsToStrings(lastIndex, setOfSets);
     }
 
-    private void getIntegers(int itemNumber, int position, int n, int lastIndex) {
-        if (itemNumber >= n || position >= (lastIndex - 1))
+    private List<String> convertSetsOfIntsToStrings(int lastIndex, Set<Set<Integer>> setOfSets) {
+        List<String> listOfStrings = new ArrayList<>();
+        for (Set<Integer> setOfInts : setOfSets) {
+            StringBuilder parenthesis = new StringBuilder();
+            for (int i = 0; i <= lastIndex; i++) {
+                parenthesis.append(setOfInts.contains(i) ? "(" : ")");
+            }
+            listOfStrings.add(parenthesis.toString());
+        }
+        return listOfStrings;
+    }
+
+    private void getIntegers(int itemNumber, int position, int n, int lastIndex, Set<Integer> set,
+                             Set<Set<Integer>> setOfSets) {
+        if (itemNumber >= n) {
+            setOfSets.add(set);
             return;
-        System.out.println("========> Item number: " + itemNumber);
+        }
         int borderIndex = lastIndex - 2 * (n - itemNumber) + 1;
+
         for (int i = position; i <= borderIndex; i++) {
-            System.out.println("Item number: " + itemNumber + " position: " + i);
-            getIntegers(itemNumber + 1, i + 1, n, lastIndex);
+            Set<Integer> newSet = new HashSet<>(set);
+            newSet.add(i);
+            getIntegers(itemNumber + 1, i + 1, n, lastIndex, newSet, setOfSets);
         }
     }
 }
